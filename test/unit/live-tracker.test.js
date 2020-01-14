1b1685311ec6d8fea75e9e13a69368ff8cb2d91b
brandonocasey <brandonocasey@gmail.com>
2020-01-14 12:04:56 -0500

    fix: liveui tweaks to prevent jitter
    add an option called liveTolerance to replace seekableInterval

--- test/unit/live-tracker.test.js
+++ test/unit/live-tracker.test.js
@@ -33,10 +33,11 @@ QUnit.module('LiveTracker', () => {
 
     this.player.duration(5);
     assert.notOk(this.liveTracker.isTracking(), 'not started');
+    assert.equal(liveEdgeChange, 2, 'liveedgechange fired when we stop tracking');
 
     this.player.duration(Infinity);
     assert.ok(this.liveTracker.isTracking(), 'started');
-    assert.equal(liveEdgeChange, 2, 'liveedgechange fired again');
+    assert.equal(liveEdgeChange, 3, 'liveedgechange fired again');
   });
 
   QUnit.module('tracking', {
@@ -49,11 +50,6 @@ QUnit.module('LiveTracker', () => {
       this.player.duration(Infinity);
 
       this.liveEdgeChanges = 0;
-      this.seekableEndChanges = 0;
-
-      this.liveTracker.on('seekableendchange', () => {
-        this.seekableEndChanges++;
-      });
 
       this.liveTracker.on('liveedgechange', () => {
         this.liveEdgeChanges++;
@@ -67,7 +63,7 @@ QUnit.module('LiveTracker', () => {
 
   QUnit.test('Triggers liveedgechange when we fall behind and catch up', function(assert) {
 
-    this.liveTracker.seekableIncrement_ = 6;
+    this.liveTracker.options_.liveTolerance = 6;
     this.player.seekable = () => createTimeRanges(0, 20);
     this.player.trigger('timeupdate');
     this.player.currentTime = () => 14;
@@ -78,7 +74,7 @@ QUnit.module('LiveTracker', () => {
     assert.equal(this.liveEdgeChanges, 1, 'should have one live edge change');
     assert.ok(this.liveTracker.behindLiveEdge(), 'behind live edge');
 
-    this.player.currentTime = () => 20;
+    this.player.currentTime = () => this.liveTracker.liveCurrentTime();
     this.clock.tick(30);
 
     assert.equal(this.liveEdgeChanges, 2, 'should have two live edge change');
@@ -86,8 +82,8 @@ QUnit.module('LiveTracker', () => {
   });
 
   QUnit.test('pastSeekEnd should update when seekable changes', function(assert) {
-    assert.strictEqual(this.liveTracker.liveCurrentTime(), 0.03, 'liveCurrentTime is now 0.03');
-    this.clock.tick(2000);
+    assert.strictEqual(this.liveTracker.liveCurrentTime(), 0, 'liveCurrentTime is now 0');
+    this.clock.tick(2010);
 
     assert.ok(this.liveTracker.pastSeekEnd() > 2, 'pastSeekEnd should be over 2s');
 
@@ -96,14 +92,13 @@ QUnit.module('LiveTracker', () => {
     this.clock.tick(30);
     assert.strictEqual(this.liveTracker.pastSeekEnd(), 0.03, 'pastSeekEnd start at 0.03 again');
     assert.strictEqual(this.liveTracker.liveCurrentTime(), 2.03, 'liveCurrentTime is now 2.03');
-    assert.equal(this.seekableEndChanges, 1, 'should be one seek end change');
   });
 
-  QUnit.test('seeks to live edge on seekableendchange', function(assert) {
+  QUnit.test('can seek to live edge', function(assert) {
     this.player.trigger('timeupdate');
 
     this.player.seekable = () => createTimeRanges(0, 6);
-    this.liveTracker.seekableIncrement_ = 2;
+    this.liveTracker.options_.liveTolerance = 2;
     let currentTime = 0;
 
     this.player.currentTime = (ct) => {
@@ -122,7 +117,7 @@ QUnit.module('LiveTracker', () => {
     assert.equal(currentTime, this.liveTracker.liveCurrentTime(), 'should have seeked to liveCurrentTime');
   });
 
-  QUnit.test('does not seek to to live edge if at live edge', function(assert) {
+  QUnit.test('does not seek to live edge if at live edge', function(assert) {
     let pauseCalls = 0;
     let playCalls = 0;
     let currentTime = 0;
@@ -146,13 +141,12 @@ QUnit.module('LiveTracker', () => {
     assert.notOk(this.player.hasClass('vjs-waiting'), 'player should not be waiting');
     assert.equal(pauseCalls, 0, 'should not have called pause');
 
-    this.clock.tick(2000);
+    this.clock.tick(2010);
 
     assert.ok(this.liveTracker.pastSeekEnd() > 2, 'pastSeekEnd should be over 2s');
     this.player.seekable = () => createTimeRanges(0, 2);
 
     this.clock.tick(30);
-    assert.equal(this.seekableEndChanges, 1, 'should be one seek end change');
     assert.equal(currentTime, 0, 'should not have seeked to seekableEnd');
     assert.equal(playCalls, 0, 'should not have called play');
   });
